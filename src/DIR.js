8d6d4a474092759ae0e1bc72054615e706420723
Anzony Rafael González Ríos <argonzalez@UANALISIS.GOB.GT>
2022-02-07 12:17:57 -0600

    actualizacion de ip, proyecto 1.7

--- src/DIR.js
+++ src/DIR.js
@@ -1,4 +1,4 @@
 module.exports = {
-    url: 'http://localhost:9080/tasks',
-    url2: 'http://localhost:9080'
+    url: 'http://172.18.230.122:9080/tasks',
+    url2: 'http://172.18.230.122:9080'
 }
