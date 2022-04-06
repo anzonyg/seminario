6ec691d2b1192b3b68084c74d2ef0bf87eb19249
agonzalez <anzonyg@live.com>
2022-04-06 10:14:52 -0600

    prueba 2

--- src/DIR.js
+++ src/DIR.js
@@ -1,6 +1,8 @@
 /*module.exports = {
     url: 'http://172.18.230.122:9080/tasks',
     url2: 'http://172.18.230.122:9080'
+
+    prueba
 }*/
 module.exports = {
     url: 'http://localhost:9080/tasks',
