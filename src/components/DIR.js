1f98e6a7b691da7bf5f5b8a602c3977de7dc5e1f
anzonyg <50388025+anzonyg@users.noreply.github.com>
2023-10-26 08:25:28 -0600

    Update DIR.js
    se agrego las rutas de produccion

--- src/components/DIR.js
+++ src/components/DIR.js
@@ -5,8 +5,15 @@ module.exports = {
 }
 */
 
-module.exports = {
+/*module.exports = {
     url: 'http://192.168.196.107:3000'
+}*/
+
+module.exports = {
+    url: 'http://44.219.50.214:3000'
 }
 
+/*module.exports = {
+    url: 'http://192.168.196.107:3000'
+}*/
 
